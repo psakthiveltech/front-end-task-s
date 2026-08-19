79a944df6a94aa01c6be2b60e223ea3dc817a4ea
sakthivel p <psakthivel.tech@gmail.com>
2026-08-19 09:03:09 +0530

    Delete 03.Javascript/05_Function directory

--- 03.Javascript/05_Function/functionhowWorks.js
+++
@@ -1,9 +0,0 @@
-function howParameterworks(l=10,k=10){
-
-  return (l+k)
-}
-howParameterworks(1)
-howParameterworks(1,2)
-howParameterworks(40,50)
-howParameterworks(1,10,1,2222)
-console.log(howParameterworks(11,1,1));
