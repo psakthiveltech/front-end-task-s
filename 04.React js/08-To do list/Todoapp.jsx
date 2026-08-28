ff28fc012336761e3dffbebe2745e774f270d355
sakthivel p <psakthivel.tech@gmail.com>
2026-08-28 09:54:17 +0530

    Add files via upload

---
+++ 04.React js/08-To do list/Todoapp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Todoapp() {
+  return (
+    <div>
+      
+      <input 
+      type="text"
+      
+      />
+      <button></button>
+    </div>
+  )
+}
+
+export default Todoapp
